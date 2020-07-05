d90f4c2341f822fe4fd7f74f7321a0b8d101f664
“Aliaksandr <aliaksandr.yanushkevich@gmail.com>
2020-07-05 18:46:07 +0300

    refactor: transform Account to pure function

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -2,18 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import AvForm from 'availity-reactstrap-validation/lib/AvForm';
 import AvField from 'availity-reactstrap-validation/lib/AvField';
+import { ToastContainer } from 'react-toastify';
 import styles from './Account.module.scss';
 import firebaseApi from '../../api/firebaseApi';
 import Button from '../common/Button/Button';
 import fields from './accountFields';
 import SubmitButton from '../common/SubmitButton/SubmitButton';
 import showToast from '../../helpers/showToast';
-import { ToastContainer } from 'react-toastify';
 
-class Account extends React.Component {
-  updatePassword = (event, errors, values) => {
+const Account = ({ firstName, lastName, role, hideAccountPage, email }) => {
+  const updatePassword = (event, errors, values) => {
     if (!errors.length) {
-      const { email } = this.props;
       const { oldPassword, password, repeatedPassword } = values;
 
       firebaseApi.updatePassword(email, oldPassword, password, repeatedPassword).then((result) => {
@@ -22,73 +21,65 @@ class Account extends React.Component {
     }
   };
 
-  render() {
-    const { firstName, lastName, role, hideAccountPage } = this.props;
-
-    const formFields = fields.map(({ id, value, name, type, label, placeholder, regexp, errorMessage }) => {
-      if (id === 'oldPassword') {
-        return (
-          <AvField
-            key={id}
-            id={id}
-            value={value}
-            name={name}
-            type={type}
-            label={label}
-            placeholder={placeholder}
-            onChange={this.onChange}
-            validate={{
-              required: { value: true, errorMessage: 'Field is required' },
-            }}
-          />
-        );
-      }
+  const formFields = fields.map(({ id, name, type, label, placeholder, regexp, errorMessage }) => {
+    if (id === 'oldPassword') {
       return (
         <AvField
           key={id}
           id={id}
-          value={value}
           name={name}
           type={type}
           label={label}
           placeholder={placeholder}
-          onChange={this.onChange}
           validate={{
             required: { value: true, errorMessage: 'Field is required' },
-            pattern: {
-              value: `${regexp}`,
-              errorMessage,
-            },
           }}
         />
       );
-    });
-
+    }
     return (
-      <>
-        <ToastContainer />
-        <div className={styles.wrapper}>
-          <h1 className={styles.title}>Account</h1>
-          <p>{`Name: ${firstName} ${lastName}`}</p>
-          <p>{`Role: ${role}`}</p>
-
-          <AvForm id='changePassword' className={styles.form} onSubmit={this.updatePassword}>
-            {formFields}
-          </AvForm>
-          <div className={styles.buttonWrapper}>
-            <SubmitButton className={styles.successButton} form='changePassword'>
-              Change
-            </SubmitButton>
-
-            <Button className={styles.defaultButton} id='backToGrid' onClick={hideAccountPage}>
-              Back to grid
-            </Button>
-          </div>
-        </div>
-      </>
+      <AvField
+        key={id}
+        id={id}
+        name={name}
+        type={type}
+        label={label}
+        placeholder={placeholder}
+        validate={{
+          required: { value: true, errorMessage: 'Field is required' },
+          pattern: {
+            value: `${regexp}`,
+            errorMessage,
+          },
+        }}
+      />
     );
-  }
-}
+  });
+
+  return (
+    <>
+      <ToastContainer />
+      <div className={styles.wrapper}>
+        <h1 className={styles.title}>Account</h1>
+        <p>{`Name: ${firstName} ${lastName}`}</p>
+        <p>{`Role: ${role}`}</p>
+
+        <AvForm id='changePassword' className={styles.form} onSubmit={updatePassword}>
+          {formFields}
+        </AvForm>
+        <div className={styles.buttonWrapper}>
+          <SubmitButton className={styles.successButton} form='changePassword'>
+            Change
+          </SubmitButton>
+
+          <Button className={styles.defaultButton} id='backToGrid' onClick={hideAccountPage}>
+            Back to grid
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
 
 Account.propTypes = {
   firstName: PropTypes.string,
